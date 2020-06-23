13c249f8a8c466607fe938fc893ce5c7b6c6b8ab
Renan Colombo <renancolombomail@gmail.com>
2020-06-23 20:45:28 -0300

    Fix undying cooldown tooltip

--- src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingUndying.java
+++ src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingUndying.java
@@ -3,11 +3,13 @@ package com.focamacho.ringsofascension.item.rings;
 import com.focamacho.ringsofascension.config.ConfigRingsOfAscension;
 import com.focamacho.ringsofascension.item.ItemRingBase;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -26,6 +28,6 @@ public class ItemRingUndying extends ItemRingBase {
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
         super.appendTooltip(stack, world, tooltip, context);
 
-        tooltip.add(new LiteralText("§e" + new TranslatableText("tooltip.ringsofascension.undying_cooldown").asString().replace("secondsHere", "§c" + ConfigRingsOfAscension.ringUndyingCooldown + "§e")));
+        tooltip.add(new LiteralText("§e" + I18n.translate("tooltip.ringsofascension.undying_cooldown").replace("secondsHere", "§c" + ConfigRingsOfAscension.ringUndyingCooldown + "§e")));
     }
 }
