a14581621a3e48593a972b9882973b489710ff0f
Focamacho <renancolombomail@gmail.com>
2020-08-04 19:43:54 -0300

    Fixes #12

--- src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingSponge.java
+++ src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingSponge.java
@@ -1,13 +1,17 @@
 package com.focamacho.ringsofascension.item.rings;
 
 import com.focamacho.ringsofascension.item.ItemRingBase;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.WaterFluid;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ItemRingSponge extends ItemRingBase {
 
@@ -19,14 +23,25 @@ public class ItemRingSponge extends ItemRingBase {
     public void tick(PlayerEntity player, ItemStack stack) {
         if(player.world.isClient || player.isSneaking()) return;
 
+        World world = player.world;
         BlockPos entityPos = player.getBlockPos();
         int range = 3;
 
         for(BlockPos pos : BlockPos.iterate(entityPos.getX() - range, entityPos.getY() - range, entityPos.getZ() - range, entityPos.getX() + range, entityPos.getY() + range, entityPos.getZ() + range)) {
-            Fluid fluid = player.world.getFluidState(pos).getFluid();
+            BlockState state = world.getBlockState(pos);
+            FluidState fluid = world.getFluidState(pos);
+            Material material = state.getMaterial();
 
-            if(fluid.equals(Fluids.WATER) || fluid.equals(Fluids.FLOWING_WATER)) {
-                player.world.setBlockState(pos, Blocks.AIR.getDefaultState());
+            if (fluid.matches(FluidTags.WATER)) {
+                if (state.getBlock() instanceof FluidDrainable && ((FluidDrainable) state.getBlock()).tryDrainFluid(world, pos, state) != Fluids.EMPTY) {
+                    continue;
+                } else if (state.getBlock() instanceof FluidBlock) {
+                    world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+                } else if (material == Material.UNDERWATER_PLANT || material == Material.REPLACEABLE_UNDERWATER_PLANT) {
+                    BlockEntity tileentity = state.getBlock().hasBlockEntity() ? world.getBlockEntity(pos) : null;
+                    Block.dropStacks(state, world, pos, tileentity);
+                    world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+                }
             }
         }
     }
