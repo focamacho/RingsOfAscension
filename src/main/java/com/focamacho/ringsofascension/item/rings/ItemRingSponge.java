972936a8a89b873b89f620ee673e020b1b4f9fc4
Focamacho <renancolombomail@gmail.com>
2020-08-04 19:28:38 -0300

    Fixes #12

--- src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingSponge.java
+++ src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingSponge.java
@@ -2,14 +2,16 @@ package com.focamacho.ringsofascension.item.rings;
 
 import com.focamacho.ringsofascension.config.ConfigHolder;
 import com.focamacho.ringsofascension.item.ItemRingBase;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
@@ -29,16 +31,26 @@ public class ItemRingSponge extends ItemRingBase {
         if(!ConfigHolder.ringSponge) return;
         if(livingEntity.world.isRemote || livingEntity.isCrouching()) return;
 
+        World world = livingEntity.world;
         BlockPos entityPos = new BlockPos(livingEntity.getPosX(), livingEntity.getPosY(), livingEntity.getPosZ());
         int range = 3;
 
         for(BlockPos pos : BlockPos.getAllInBoxMutable(entityPos.getX() - range, entityPos.getY() - range, entityPos.getZ() - range, entityPos.getX() + range, entityPos.getY() + range, entityPos.getZ() + range)) {
-            Fluid fluid = livingEntity.world.getFluidState(pos).getFluid();
+            BlockState state = livingEntity.world.getBlockState(pos);
+            FluidState fluid = livingEntity.world.getFluidState(pos);
+            Material material = state.getMaterial();
 
-            if(fluid.equals(Fluids.WATER) || fluid.equals(Fluids.FLOWING_WATER)) {
-                livingEntity.world.setBlockState(pos, Blocks.AIR.getDefaultState());
+            if (fluid.isTagged(FluidTags.WATER)) {
+                if (state.getBlock() instanceof IBucketPickupHandler && ((IBucketPickupHandler) state.getBlock()).pickupFluid(world, pos, state) != Fluids.EMPTY) {
+                    continue;
+                } else if (state.getBlock() instanceof FlowingFluidBlock) {
+                    world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+                } else if (material == Material.OCEAN_PLANT || material == Material.SEA_GRASS) {
+                    TileEntity tileentity = state.getBlock().hasTileEntity(state) ? world.getTileEntity(pos) : null;
+                    Block.spawnDrops(state, world, pos, tileentity);
+                    world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+                }
             }
-
         }
     }
 
