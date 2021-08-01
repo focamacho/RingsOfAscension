d4b89c6f79fd0d2108d91c0424e6caa11cc40c11
Focamacho <renancolombomail@gmail.com>
2021-08-01 12:19:35 -0300

    Use PlayerAbilityLib to avoid conflicts

--- src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingFlight.java
+++ src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingFlight.java
@@ -2,11 +2,16 @@ package com.focamacho.ringsofascension.item.rings;
 
 import com.focamacho.ringsofascension.item.ItemRingBase;
 import dev.emi.trinkets.api.SlotReference;
+import io.github.ladysnake.pal.AbilitySource;
+import io.github.ladysnake.pal.Pal;
+import io.github.ladysnake.pal.VanillaAbilities;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 
 public class ItemRingFlight extends ItemRingBase {
+
+    public static final AbilitySource abilityFlightRing = Pal.getAbilitySource("ringsofascension", "ring_flight");
 
     public ItemRingFlight(String name, int tier, String tooltip, boolean enabled, String locations) {
         super(name, tier, tooltip, enabled, locations);
@@ -14,28 +19,15 @@ public class ItemRingFlight extends ItemRingBase {
 
     @Override
     public void onEquip(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        if(entity instanceof PlayerEntity player && !player.isCreative()) {
-            player.getAbilities().allowFlying = true;
-            player.getAbilities().flying = true;
-            player.sendAbilitiesUpdate();
-        }
-    }
-
-    @Override
-    public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        super.tick(stack, slot, entity);
-        if(entity instanceof PlayerEntity player && !player.isCreative()) {
-            player.getAbilities().allowFlying = true;
-            player.sendAbilitiesUpdate();
+        if(entity instanceof PlayerEntity player && !player.isCreative() && !entity.getEntityWorld().isClient()) {
+            abilityFlightRing.grantTo(player, VanillaAbilities.ALLOW_FLYING);
         }
     }
 
     @Override
     public void onUnequip(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        if(entity instanceof PlayerEntity player && !player.isCreative()) {
-            player.getAbilities().allowFlying = false;
-            player.getAbilities().flying = false;
-            player.sendAbilitiesUpdate();
+        if(entity instanceof PlayerEntity player && !player.isCreative() && !entity.getEntityWorld().isClient()) {
+            abilityFlightRing.revokeFrom(player, VanillaAbilities.ALLOW_FLYING);
         }
     }
 
