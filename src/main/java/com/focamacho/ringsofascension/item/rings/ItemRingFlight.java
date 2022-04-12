a4e27222800ce15e32f9b973402240368483c03a
Focamacho <48870573+focamacho@users.noreply.github.com>
2022-04-12 10:26:10 -0300

    Remove PlayerAbilityLib dependency

--- src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingFlight.java
+++ src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingFlight.java
@@ -2,16 +2,11 @@ package com.focamacho.ringsofascension.item.rings;
 
 import com.focamacho.ringsofascension.item.ItemRingBase;
 import dev.emi.trinkets.api.SlotReference;
-import io.github.ladysnake.pal.AbilitySource;
-import io.github.ladysnake.pal.Pal;
-import io.github.ladysnake.pal.VanillaAbilities;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 
 public class ItemRingFlight extends ItemRingBase {
-
-    public static final AbilitySource abilityFlightRing = Pal.getAbilitySource("ringsofascension", "ring_flight");
 
     public ItemRingFlight(String name, int tier, String tooltip, boolean enabled, String locations) {
         super(name, tier, tooltip, enabled, locations);
