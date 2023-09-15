2db5112a2bab5bf6a34c34dca668bea1bb970663
focamacho <renancolombomail@gmail.com>
2023-09-15 05:26:35 -0300

    style: organize imports

--- src/main/java/com/focamacho/ringsofascension/utils/Utils.java
+++ src/main/java/com/focamacho/ringsofascension/utils/Utils.java
@@ -2,7 +2,6 @@ package com.focamacho.ringsofascension.utils;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.CuriosApi;
 
 public class Utils {
