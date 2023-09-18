c573e53adb4ff206952bf915f307aa952a57c77b
focamacho <renancolombomail@gmail.com>
2023-09-18 00:57:46 -0300

    refactor: improve ring of walter walking

--- src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingWaterWalking.java
+++ src/main/java/com/focamacho/ringsofascension/item/rings/ItemRingWaterWalking.java
@@ -5,6 +5,7 @@ import com.focamacho.ringsofascension.item.ItemRingBase;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.Vec3;
 
@@ -19,15 +20,19 @@ public class ItemRingWaterWalking extends ItemRingBase {
         if(!isEnabled) return;
         if(!(livingEntity instanceof Player) || livingEntity.isCrouching()) return;
 
-        BlockPos entityPos = new BlockPos(livingEntity.getBlockX(), livingEntity.getBlockY(), livingEntity.getBlockZ());
+        BlockPos entityPos = livingEntity.blockPosition();
 
-        boolean water = livingEntity.level.getBlockState(new BlockPos(entityPos.getX(), (int) Math.floor(livingEntity.getBoundingBox().minY - 0.1), entityPos.getZ())).getMaterial() == Material.WATER;
+        boolean water =
+                livingEntity.level.getFluidState(
+                        new BlockPos(entityPos.getX(), (int) Math.floor(livingEntity.getBoundingBox().minY - 0.4), entityPos.getZ())
+                ).is(Fluids.WATER);
 
-        if(water && !livingEntity.isInWater() && livingEntity.getDeltaMovement().y < 0.0D) {
+        if(water && !livingEntity.isInWater()) {
             Vec3 motion = livingEntity.getDeltaMovement();
 
             livingEntity.setDeltaMovement(motion.x, 0, motion.z);
             livingEntity.fallDistance = 0;
+            livingEntity.setOnGround(true);
         }
     }
 
