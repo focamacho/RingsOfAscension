1ee296dfb617885a9268e35fe480223d26d88e05
focamacho <renancolombomail@gmail.com>
2023-09-19 11:43:26 -0300

    fix: slot tooltip

--- src/main/java/com/focamacho/ringsofascension/item/ItemRingBase.java
+++ src/main/java/com/focamacho/ringsofascension/item/ItemRingBase.java
@@ -50,7 +50,9 @@ public class ItemRingBase extends TrinketItem {
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
         super.appendTooltip(stack, world, tooltip, context);
 
-        tooltip.add(Text.translatable("tooltip.ringsofascension.slot").formatted(Formatting.GRAY).append(Text.literal(" ")).append(Text.translatable("tooltip.ringsofascension.slot.ring").formatted(Formatting.YELLOW)));
+        tooltip.add(
+                Text.translatable("tooltip.ringsofascension.slot").formatted(Formatting.GRAY)
+                        .append(Text.translatable("tooltip.ringsofascension.ring").formatted(Formatting.YELLOW)));
 
         tooltip.add(Text.literal(""));
         tooltip.add(Text.translatable("tooltip.ringsofascension.worn").formatted(Formatting.GRAY));
