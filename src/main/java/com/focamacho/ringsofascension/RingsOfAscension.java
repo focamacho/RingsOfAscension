78040a96d825b5285c8628f68ba79a7212332959
focamacho <renancolombomail@gmail.com>
2023-09-21 10:00:17 -0300

    feat: loot modifiers

--- src/main/java/com/focamacho/ringsofascension/RingsOfAscension.java
+++ src/main/java/com/focamacho/ringsofascension/RingsOfAscension.java
@@ -1,18 +1,42 @@
 package com.focamacho.ringsofascension;
 
 import com.focamacho.ringsofascension.config.ConfigRingsOfAscension;
+import com.focamacho.ringsofascension.data.LootModifier;
 import com.focamacho.ringsofascension.init.ModItems;
 import com.focamacho.sealconfig.SealConfig;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.LootCondition;
+import net.minecraft.loot.condition.LootConditionTypes;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.registry.Registries;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RingsOfAscension implements ModInitializer {
 
@@ -26,14 +50,88 @@ public class RingsOfAscension implements ModInitializer {
 			.icon(() -> new ItemStack(ModItems.ringExperience))
 			.build();
 
+	private static final List<LootModifier> LOOT_MODIFIERS = new ArrayList<>();
+
 	@Override
 	public void onInitialize() {
 		config = sealConfig.getConfig(new File(FabricLoader.getInstance().getConfigDir().toFile(), "ringsofascension.json"), ConfigRingsOfAscension.class);
 		ModItems.init();
 
+		// Creative Tab
 		ItemGroupEvents.modifyEntriesEvent(CREATIVE_TAB).register(content ->
 				ModItems.allRings.forEach(r -> content.add(new ItemStack(r)))
 		);
+
+		// Loot Modifiers
+		ResourceManagerHelper.get(
+				ResourceType.SERVER_DATA
+		).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
+
+			@Override
+			public void reload(ResourceManager manager) {
+				loadModifiers(manager);
+			}
+
+			@Override
+			public Identifier getFabricId() {
+				return new Identifier(MODID, "loot_modifiers");
+			}
+
+		});
+
+		// Change Loot Tables to use Loot Modifiers
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if(LOOT_MODIFIERS.isEmpty()) loadModifiers(resourceManager);
+
+			LOOT_MODIFIERS.stream().filter(mod ->
+					ArrayUtils.contains(mod.loot_tables, id.toString())
+			).forEach(modifier ->
+					tableBuilder.pool(
+							new LootPool.Builder()
+									.conditionally(Arrays.stream(modifier.conditions).collect(Collectors.toList()))
+									.with(
+											ItemEntry.builder(
+													modifier.ring
+											)
+									)
+									.build()
+					));
+
+		});
+
+	}
+
+	private void loadModifiers(ResourceManager manager) {
+		LOOT_MODIFIERS.clear();
+
+		Gson gson = new GsonBuilder().registerTypeAdapter(
+				TypeToken.get(LootCondition.class).getType(),
+				LootConditionTypes.createGsonSerializer()
+		).registerTypeAdapter(
+				TypeToken.get(Item.class).getType(),
+				(JsonSerializer<Item>) (src, typeOfSrc, context) -> new JsonPrimitive(Registries.ITEM.getId(src).toString())
+		).registerTypeAdapter(
+				TypeToken.get(Item.class).getType(),
+				(JsonDeserializer<Item>) (json, typeOfT, context) -> {
+					String[] identifier = json.getAsString().split(":");
+					return Registries.ITEM.get(
+							Identifier.of(identifier[0], identifier[1])
+					);
+				}
+		).create();
+
+		Map<Identifier, Resource> resources = manager.findResources("loot_modifiers", (identifier) -> identifier.getPath().endsWith(".json"));
+		for (Map.Entry<Identifier, Resource> entry : resources.entrySet()) {
+			try(InputStream input = entry.getValue().getInputStream()) {
+				LootModifier modifier = gson.fromJson(
+						new String(input.readAllBytes(), StandardCharsets.UTF_8),
+						LootModifier.class
+				);
+				LOOT_MODIFIERS.add(modifier);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
---
+++ src/main/java/com/focamacho/ringsofascension/data/LootModifier.java
@@ -0,0 +1,13 @@
+package com.focamacho.ringsofascension.data;
+
+import net.minecraft.item.Item;
+import net.minecraft.loot.condition.LootCondition;
+
+public class LootModifier {
+
+    public String type;
+    public LootCondition[] conditions;
+    public String[] loot_tables;
+    public Item ring;
+
+}
